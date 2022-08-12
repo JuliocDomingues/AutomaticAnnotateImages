0ace2b5e8fdd0b16a6c3762e6f21e7459b532fef
Julio César Domingues dos Santos - Estágio SST <estagio.sst17@sesims.com.br>
2022-08-12 10:21:28 -0400

    Reorganização da estrutura do arquivo main.py

--- main.py
+++ main.py
@@ -3,5 +3,13 @@ from yolov5.model import load_model
 
 model = load_model(weights=r'C:\Users\estagio.sst17\source\repos\AnnotateImages\NeuralNetworkModels\yolov5x.pt')
 
-result = run(source=r'C:\Users\estagio.sst17\OneDrive - SESIMS\Documentos\Banco', model=model, save_txt=True, conf_thres=0.65)
 
+def call_annotate():
+    return run(source=r'C:\Users\estagio.sst17\OneDrive - SESIMS\Documentos\Banco',
+               model=model,
+               save_txt=True,
+               conf_thres=0.65)
+
+
+if __name__ == '__main__':
+    call_annotate()
